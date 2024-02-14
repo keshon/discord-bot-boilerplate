1cc6d26989f3635b2efa466bc4a87a816a6136cb
Innokentiy Sokolov <keshon@zoho.com>
2024-02-14 10:47:18 +0300

    fix: refactor

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"net"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -19,39 +19,48 @@ import (
 	"github.com/keshon/discord-bot-boilerplate/internal/version"
 )
 
+// main is the entry point of the program.
+//
+// There are no parameters.
+// There is no return type.
 func main() {
 	initLogger()
-
-	config := initConfig()
-
+	config := loadConfig()
 	initDatabase()
-
-	dg := initDiscordSession(config.DiscordBotToken)
-
-	bots := initBots(dg)
-
-	handleDiscordSession(dg)
-
+	discordSession := createDiscordSession(config.DiscordBotToken)
+	bots := startBots(discordSession)
+	handleDiscordSession(discordSession)
 	startRestServer(config, bots)
-
 	slog.Infof("%v is now running. Press Ctrl+C to exit", version.AppName)
-
 	waitForExitSignal()
 }
 
+// initLogger initializes the logger with color theme and file handler.
+//
+// No parameters.
+// No return type.
 func initLogger() {
 	slog.Configure(func(logger *slog.SugaredLogger) {
-		f := logger.Formatter.(*slog.TextFormatter)
-		f.EnableColor = true
-		f.SetTemplate("[{{datetime}}] [{{level}}] [{{caller}}]\t{{message}} {{data}} {{extra}}\n")
-		f.ColorTheme = slog.ColorTheme
+		if textFormatter, ok := logger.Formatter.(*slog.TextFormatter); ok {
+			textFormatter.EnableColor = true
+			textFormatter.SetTemplate("[{{datetime}}] [{{level}}] [{{caller}}]\t{{message}} {{data}} {{extra}}\n")
+			textFormatter.ColorTheme = slog.ColorTheme
+		} else {
+			slog.Error("Error: Text formatter is not a *slog.TextFormatter")
+		}
 	})
-
-	h1 := handler.MustFileHandler("./logs/all-levels.log", handler.WithLogLevels(slog.AllLevels))
-	slog.PushHandler(h1)
+	if fileHandler, err := handler.NewFileHandler("./logs/all-levels.log", handler.WithLogLevels(slog.AllLevels)); err != nil {
+		slog.Error("Error creating file handler:", err)
+	} else {
+		slog.PushHandler(fileHandler)
+	}
 }
 
-func initConfig() *config.Config {
+// loadConfig loads the configuration and returns a pointer to config.Config.
+//
+// No parameters.
+// Returns *config.Config.
+func loadConfig() *config.Config {
 	cfg, err := config.NewConfig()
 	if err != nil {
 		slog.Fatal("Error loading config", err)
@@ -61,6 +70,10 @@ func initConfig() *config.Config {
 	return cfg
 }
 
+// initDatabase initializes the database.
+//
+// No parameters.
+// No return type.
 func initDatabase() {
 	_, err := db.InitDB("./dbase.db")
 	if err != nil {
@@ -69,90 +82,83 @@ func initDatabase() {
 	}
 }
 
-func initDiscordSession(token string) *discordgo.Session {
-	dg, err := discordgo.New("Bot " + token)
+// createDiscordSession creates a new Discord session using the provided token.
+//
+// It takes a string token as a parameter and returns a *discordgo.Session.
+func createDiscordSession(token string) *discordgo.Session {
+	session, err := discordgo.New("Bot " + token)
 	if err != nil {
-		slog.Fatal("Error creating Discord session", err)
-		os.Exit(1)
+		log.Fatal("Error creating Discord session", err)
 	}
-	return dg
+	return session
 }
 
-func initBots(session *discordgo.Session) map[string]*discord.BotInstance {
+// startBots creates and starts Discord bots for each guild using the given session.
+//
+// session *discordgo.Session
+// map[string]*discord.BotInstance
+func startBots(session *discordgo.Session) map[string]*discord.BotInstance {
 	bots := make(map[string]*discord.BotInstance)
-
 	guildManager := manager.NewGuildManager(session, bots)
 	guildManager.Start()
-
 	guildIDs, err := db.GetAllGuildIDs()
 	if err != nil {
 		slog.Fatal("Error retrieving or creating guilds", err)
-		os.Exit(1)
 	}
-
 	for _, guildID := range guildIDs {
-		bots = startBotInstances(session, guildID, bots)
+		instance := discord.NewDiscord(session, guildID)
+		bots[guildID] = &discord.BotInstance{
+			ExampleBot: instance,
+		}
+		instance.Start(guildID)
 	}
-
 	return bots
 }
 
-func handleDiscordSession(dg *discordgo.Session) {
-	err := dg.Open()
-	if err != nil {
+// handleDiscordSession is a Go function that opens a Discord session and handles any errors.
+//
+// It takes a parameter discordSession of type *discordgo.Session.
+// It does not return any value.
+func handleDiscordSession(discordSession *discordgo.Session) {
+	if err := discordSession.Open(); err != nil {
 		slog.Fatal("Error opening Discord session", err)
 		os.Exit(1)
 	}
-	defer dg.Close()
+	defer discordSession.Close()
 }
 
-func startBotInstances(session *discordgo.Session, guildID string, bots map[string]*discord.BotInstance) map[string]*discord.BotInstance {
-	bots[guildID] = &discord.BotInstance{
-		ExampleBot: discord.NewDiscord(session, guildID),
-	}
-	bots[guildID].ExampleBot.Start(guildID)
-
-	return bots
-}
-
+// startRestServer starts the REST server based on the given configuration and bot instances.
+//
+// It takes a config.Config pointer and a map of string to *discord.BotInstance as parameters.
 func startRestServer(config *config.Config, bots map[string]*discord.BotInstance) {
 	if !config.RestEnabled {
 		return
 	}
-
 	if config.RestGinRelease {
 		gin.SetMode("release")
 	}
-
 	router := gin.Default()
-
 	restAPI := rest.NewRest(bots)
 	restAPI.Start(router)
-
 	go func() {
-		hostname := config.RestHostname
-
-		if len(hostname) == 0 {
-			hostname = "localhost:8080"
-			slog.Warn("Hostname is empty, setting to default", hostname)
+		if len(config.RestHostname) == 0 {
+			config.RestHostname = "localhost:8080"
+			slog.Warn("Hostname is empty, setting to default:", config.RestHostname)
 		}
-
-		_, _, err := net.SplitHostPort(hostname)
-		if err != nil {
-			slog.Error("Could not detect host and port from sring", hostname)
+		if err := router.Run(config.RestHostname); err != nil {
+			slog.Fatal("Error starting REST API server:", err)
 			return
 		}
-
-		if err := router.Run(hostname); err != nil {
-			slog.Fatal("Error starting REST API server", err)
-			return
-		}
-		slog.Info("REST API server started on", hostname)
+		slog.Info("REST API server started on", config.RestHostname)
 	}()
 }
 
+// waitForExitSignal waits for an exit signal and handles it.
+//
+// No parameters.
+// No return types.
 func waitForExitSignal() {
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
+	exitSignalChannel := make(chan os.Signal, 1)
+	signal.Notify(exitSignalChannel, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-exitSignalChannel
 }
